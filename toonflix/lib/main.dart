c8caabda2ebdf5fcb6060f0ec17eb229d5680a42
dev-hannahk <dev.hannahk@gmail.com>
2023-09-19 21:24:13 +0900

    Create Custom Header

--- toonflix/lib/main.dart
+++ toonflix/lib/main.dart
@@ -11,15 +11,43 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     // root must choose material or cupertino(ios)
     return MaterialApp(
-      // Scaffold is widget whic has nice structure and screen
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Hello, Flutter!"),
-        ),
-        body: Center(
-          child: Text("Hello World"),
-        ),
-      ),
-    );
+        // Scaffold is widget whic has nice structure and screen
+        home: Scaffold(
+      backgroundColor: Color(0xFF181818),
+      body: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: 20,
+          ),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      SizedBox(
+                        height: 80,
+                      ),
+                      Text(
+                        'Hey, Selena',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 28,
+                          fontWeight: FontWeight.w800,
+                        ),
+                      ),
+                      Text(
+                        'Welcome Back',
+                        style: TextStyle(
+                            color: Colors.white.withOpacity(0.8), fontSize: 16),
+                      ),
+                    ],
+                  )
+                ],
+              )
+            ],
+          )),
+    ));
   }
 }
